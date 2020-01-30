62641aca1ef7fd37f89e726fb2a877a915e333fb
kjmiyata <kevinmiyata@discover.com>
2020-01-30 14:45:05 -0600

    enable eureka server

--- platform-services/eureka-server/src/main/kotlin/io/pivotal/pal/tracker/eurekaserver/App.kt
+++ platform-services/eureka-server/src/main/kotlin/io/pivotal/pal/tracker/eurekaserver/App.kt
@@ -2,7 +2,9 @@ package io.pivotal.pal.tracker.eurekaserver
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaServer
 @SpringBootApplication
 open class App
 
