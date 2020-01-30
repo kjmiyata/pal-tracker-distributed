b357dca175d2a469570341635cfa582e6096869c
kjmiyata <kevinmiyata@discover.com>
2020-01-30 15:10:02 -0600

    fix config

---
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/SecurityConfiguration.kt
@@ -0,0 +1,14 @@
+package io.pivotal.pal.tracker.backlog
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+open class SecurityConfiguration(disableDefaults: Boolean = false) : WebSecurityConfigurerAdapter(disableDefaults) {
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable()
+    }
+}
--- platform-services/eureka-server/src/main/kotlin/io/pivotal/pal/tracker/eurekaserver/App.kt
+++ platform-services/eureka-server/src/main/kotlin/io/pivotal/pal/tracker/eurekaserver/App.kt
@@ -2,8 +2,10 @@ package io.pivotal.pal.tracker.eurekaserver
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaClient
 @EnableEurekaServer
 @SpringBootApplication
 open class App
